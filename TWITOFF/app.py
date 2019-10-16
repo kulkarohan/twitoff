59fa8dabaf0fb0ccb500e0cfc092227e534c7f7a
“kulkarohan” <kulkarohan@users.noreply.github.com>
2019-10-16 18:04:17 -0400

    added user search bar

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -5,7 +5,7 @@ Main application for twitoff
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
-
+from .twitter import add_or_update_user
 
 def create_app():
     """
@@ -14,7 +14,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
-    app.config['ENV'] = config('ENV') # change later to production
+    # app.config['ENV'] = config('ENV')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     DB.init_app(app)
@@ -23,6 +23,21 @@ def create_app():
     def root():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
+
+    @app.route('/user', methods=['POST', 'GET'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == "POST":
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+            
+        return render_template('user.html', title=name, tweets=tweets, message=message)
 
     @app.route('/reset')
     def reset():
